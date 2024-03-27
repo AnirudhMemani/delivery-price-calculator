84eb2e20925cf9c4c6fec46b6d0ca8cd1f023f6b
Anirudh Memani <nemanianirudh@gmail.com>
2024-03-27 22:05:56 +0530

    first commit

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import pkg from "pg";
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+});
+
+export default pool;
